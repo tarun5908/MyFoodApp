c2e5c1c965ad83bdfd981dc2b2bace4fa00770df
Tarun <nagatarunvadapalli@gmail.com>
2025-11-06 13:48:02 +0530

    game

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
 
 function Contact() {
-  const [gender, setGender] = useState("");
+  const [game, setGame] = useState("");
 
   const handleChange = (e) => {
-    // Set gender to the selected checkbox value
-    setGender(e.target.value);
+    // Set game to the selected dropdown value
+    setGame(e.target.value);
   };
 
   return (
     <div style={{ padding: "20px" }}>
       <select
-        value={gender}
+        value={game}
         onChange={handleChange}
         style={{ padding: "5px", width: "200px" }}
       >
-        <option value="">-- Select Gender --</option>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-        <option value="Other">Other</option>
+        <option value="">-- Select Game --</option>
+        <option value="Cricket">Cricket</option>
+        <option value="Football">Football</option>
+        <option value="Golf">Golf</option>
+        <option value="Badminton">Badminton</option>
       </select>
       <br />
 
       <input
         type="text"
-        placeholder="Selected gender will appear here"
-        value={gender}
+        placeholder="Selected game will appear here"
+        value={game}
         readOnly
         style={{
           borderBlockStyle: "solid",
